b765f60c7ce33a7df8d8272d36f06ec011907fca
Cidade Sonho <atendimento@cidadesonho.com>
2014-07-21 18:44:29 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+<div class="jumbotron">
+      <div class="container">
+        <h1><?php bloginfo( 'description' ); ?></h1>
+        <p><?php bloginfo( 'admin_email' ); ?></p>
+      </div>
+    </div>
+
+<?php get_footer(); ?>
